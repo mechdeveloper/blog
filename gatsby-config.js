8948af094ca4b0e28365cc85c5853e43794db23e
Ashish Singh Baghel <business.tech.asb@gmail.com>
2019-12-08 15:34:01 +0100

    updated site details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Tech Starter Blog`,
+    author: `Ashish Singh Baghel`,
+    description: `A starter blog by Ashish Singh Baghel`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `mechDeveloper1`,
     },
   },
-  plugins: [`gatsby-plugin-netlify-cms`,
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/layout.js
+++ src/components/layout.js
@@ -63,7 +63,7 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()} Ashish Singh Baghel, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
